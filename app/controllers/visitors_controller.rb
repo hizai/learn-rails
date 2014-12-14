80d7f8205c7fa3c2aeeb00cdfb066410bb57485b
Haritz <haritziturbe@gmail.com>
2014-12-15 00:23:37 +0100

    update application layout

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -2,6 +2,8 @@ class VisitorsController < ApplicationController
 
 	def new
 		@owner = Owner.new
+		flash.now[:notice] = 'Ongi etorri!'
+		flash.now[:alert] = 'Nire urtebetetzea laister izango da.'
 	end
 
 end
