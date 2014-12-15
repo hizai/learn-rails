ee528885be9e53d3ea516a0a7de2139eec2ee7f1
Haritz <haritziturbe@gmail.com>
2014-12-15 11:43:30 +0100

    front-end framework

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -2,8 +2,6 @@ class VisitorsController < ApplicationController
 
 	def new
 		@owner = Owner.new
-		flash.now[:notice] = 'Ongi etorri!'
-		flash.now[:alert] = 'Nire urtebetetzea laister izango da.'
 	end
 
 end
